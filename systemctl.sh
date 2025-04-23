788488786d2f6205a60ea1b260527292ab33a8f0
William <126926699+GitXpresso@users.noreply.github.com>
2025-04-23 15:40:25 +0000

    Create systemctl.sh

---
+++ systemctl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/refs/heads/master/files/docker/systemctl3.py && sudo chmod -R 755 /bin/systemctl
+
+echo -e "\n|---------------------|\n| systemctl installed!\n|---------------------|"
+
+sleep 1
+clear
+echo "do \"/bin/systemctl\" to run systemctl!"
+
